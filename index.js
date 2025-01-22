9188fa47e501c3f87aa29ef8ed7efb5fba564963
Salidjanova <ominasalidjanova@gmail.com>
2025-01-22 16:42:42 +0500

    HAmma topshiriqlar bajarildi 8-dan tashqari.

---
+++ index.js
@@ -0,0 +1,26 @@
+console.log("12-topshiriq");
+
+let ism = 'Omina'
+let yosh = 15
+let ielts = '9 ball'
+console.log( ism, yosh, ielts );
+
+
+console.log("14-topshiriq");
+function talaba() {
+    let talabaIsmi = prompt()
+    console.log(talabaIsmi);
+}
+talaba()
+
+console.log("15-topshiriq");
+let user = {
+    F_I_Sh: `Salidjanoba Omina Rustamovna`,
+    yil: 2009,
+    yosh: 15
+}
+console.log(user);
+
+
+
+
